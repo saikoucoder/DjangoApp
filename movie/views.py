719a6061b53a079568d3618312a0273e99c8bc9b
Tan Dung <tandung12072002@gmail.com>
2022-11-14 01:05:47 +0700

    search

--- movie/views.py
+++ movie/views.py
@@ -2,12 +2,22 @@ from urllib import response
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Movie
+from django.db.models import Q
 # Create your views here.
 
 
 def index(request):
     Data = {"Movies": Movie.objects.all()}
     return render(request, "pages/home.html", Data)
+
+
+def search(request):
+    if request.method == 'GET':
+        search = request.GET.get('search')
+        Data = Movie.objects.filter(Q(name__icontains=search))
+        return render(request, "pages/search.html", {'Movies': Data, 'search': search})
+    else:
+        return render(request, "pages/home.html", {})
 
 
 def getMovieById(request, id):
