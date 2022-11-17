6deea4b4edd82ed7878778f719c8384c27784ad9
Tan Dung <tandung12072002@gmail.com>
2022-11-18 00:38:52 +0700

    loại bỏ hardcode

--- movie/urls.py
+++ movie/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="home"),
-    path('search', views.search),
-    path('type', views.getType),
-    path('movie/<int:id>', views.getMovieById, name="movie"),
+    path('', views.index, name='home'),
+    path('search', views.search, name='search'),
+    path('type', views.getType, name='type'),
+    path('movie/<int:id>', views.getMovieById, name='movie'),
     path('watchmovie/<int:id>', views.watchMovie),
 ]
