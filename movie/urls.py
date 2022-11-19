f96045c2739bc7ffb496dbb371a4e5f9c7d8c731
zhukyu <phamtienhuu562002@gmail.com>
2022-11-19 14:35:18 +0700

    auth system

--- movie/urls.py
+++ movie/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.index, name="home"),
@@ -7,4 +8,7 @@ urlpatterns = [
     path('type', views.getType),
     path('movie/<int:id>', views.getMovieById, name="movie"),
     path('watchmovie/<int:id>', views.watchMovie),
+    path('register/', views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name="pages/login.html"), name="login"),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name="logout"),
 ]
